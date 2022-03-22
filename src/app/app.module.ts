c65b144cc23217cd41ccbf75ce69732ca11632b8
Patryk Zabłocki <bagnus1997@gmail.com>
2022-03-22 19:24:49 +0100

    Navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { PostsComponent } from './posts/posts.component';
 import { TodosComponent } from './todos/todos.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { TodosComponent } from './todos/todos.component';
     HomeComponent,
     UsersComponent,
     PostsComponent,
-    TodosComponent
+    TodosComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
